ccbfbd7c4ca9e848ae9338597adac0702f1d1d44
meneerkiwi <jordieclasie@hotmail.com>
2018-01-18 21:23:27 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 
-const TOKEN = "NDAzMTIxOTIzODY3MDE3MjI2.DUFDLA.BiJU228sx12ban29ZJuONhcjMPw";  
+const TOKEN = "bottoken";  
 const PREFIX = "f!";
 
 var fortunes = [
@@ -13,7 +13,7 @@ var bot = new Discord.Client();
 
 bot.on("ready", function() {
     console.log("ready");
-    bot.user.setGame("Eating frikandels || f!help");
+    bot.user.setActivity("Eating frikandels || f!help");
 });
 
 bot.on("message", function(message) {
@@ -25,20 +25,25 @@ bot.on("message", function(message) {
 
     switch (args[0].toLowerCase()) {
         case "help":
-            message.channel.sendMessage("The command prefix is '!' the commands are: ping, eat, 8ball")
+            var embed = new Discord.RichEmbed()
+                .setTitle("Prefix is f!")
+                .addField("!8ball", "ask the ball a question to give answer on.")
+                .addField("f!eat", "Eat a frikandel.")
+                .addField("f!ping", "get a response from the bot.")
+            message.channel.send(embed);
             break;
         case "ping":
-            message.channel.sendMessage("Pong!");
+            message.channel.send("Pong! and 16 ms latency");
             break;
         case "eat":
-            message.channel.sendMessage("You ate a frikandel.");
+            message.channel.send("You ate a frikandel");
             break;
         case "8ball":
-            if (args[1]) message.channel.sendMessage(fortunes[Math.floor(Math.random() * fortunes.length)]); 
-                else message.channel.sendMessage("Can't read that");
-                break;
+            if (args[1]) message.channel.send(fortunes[Math.floor(Math.random() * fortunes.length)]); 
+            else message.channel.send("Can't read that");
+            break;
         default:
-            message.channel.sendMessage("invalid command, use f!help");
+            message.channel.send("invalid command, use f!help");
     }
 });
 

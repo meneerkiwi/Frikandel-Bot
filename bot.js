c980bb5f28688a5c8705f85d7925f3fa11e9d13a
meneerkiwi <jordieclasie@hotmail.com>
2018-01-19 12:50:39 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,26 +4,26 @@ const TOKEN = "bottoken";
 const PREFIX = "f!";
 
 var fortunes = [
-    "8ball: It is certain✔",
-    "8ball: It is decidedly so✔",
-    "8ball: Without a doubt✔",
-    "8ball: definitely✔",
-    "8ball: You may rely on it✔",
-    "8ball: As I see it, yes✔",
-    "8ball: Most likely✔",
-    "8ball: Outlook good✔",
-    "8ball: Yes✔",
-    "8ball: Signs point to yes✔",
-    "8ball: Reply hazy, try again●",
-    "8ball: Ask again later●",
-    "8ball: Better not tell you now●",
-    "8ball: Cannot predict now●",
-    "8ball: Concentrate and ask again●",
-    "8ball: Don't count on itX",
-    "8ball: My reply is noX",
-    "8ball: My sources say noX",
-    "8ball: Outlook not so goodX",
-    "8ball: Very doubtfulX"
+    "8ball: It is certain ✔",
+    "8ball: It is decidedly so ✔",
+    "8ball: Without a doubt ✔",
+    "8ball: definitely ✔",
+    "8ball: You may rely on it ✔",
+    "8ball: As I see it, yes ✔",
+    "8ball: Most likely ✔",
+    "8ball: Outlook good ✔",
+    "8ball: Yes ✔",
+    "8ball: Signs point to yes ✔",
+    "8ball: Reply hazy, try again ●",
+    "8ball: Ask again later ●",
+    "8ball: Better not tell you now ●",
+    "8ball: Cannot predict now ●",
+    "8ball: Concentrate and ask again ●",
+    "8ball: Don't count on it X",
+    "8ball: My reply is no X",
+    "8ball: My sources say no X",
+    "8ball: Outlook not so good X",
+    "8ball: Very doubtful X"
 ]
 
 var bot = new Discord.Client();

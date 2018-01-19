c7a15432a7f5e0d6c2341fbb3ee82b9a11a428a5
meneerkiwi <jordieclasie@hotmail.com>
2018-01-19 12:48:58 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,13 +4,26 @@ const TOKEN = "bottoken";
 const PREFIX = "f!";
 
 var fortunes = [
-    "8ball: yes",
-    "8ball: no",
-    "8ball: maybe",
-    "8ball: definitely",
-    "8ball: My sources say no",
-    "8ball: Very doubtful",
-    "8ball: Most likely"
+    "8ball: It is certain✔",
+    "8ball: It is decidedly so✔",
+    "8ball: Without a doubt✔",
+    "8ball: definitely✔",
+    "8ball: You may rely on it✔",
+    "8ball: As I see it, yes✔",
+    "8ball: Most likely✔",
+    "8ball: Outlook good✔",
+    "8ball: Yes✔",
+    "8ball: Signs point to yes✔",
+    "8ball: Reply hazy, try again●",
+    "8ball: Ask again later●",
+    "8ball: Better not tell you now●",
+    "8ball: Cannot predict now●",
+    "8ball: Concentrate and ask again●",
+    "8ball: Don't count on itX",
+    "8ball: My reply is noX",
+    "8ball: My sources say noX",
+    "8ball: Outlook not so goodX",
+    "8ball: Very doubtfulX"
 ]
 
 var bot = new Discord.Client();
@@ -31,9 +44,9 @@ bot.on("message", function(message) {
         case "help":
             var embed = new Discord.RichEmbed()
                 .setTitle("Prefix is f!")
-                .addField("!8ball", "ask the ball a question to give answer on.")
                 .addField("f!eat", "Eat a frikandel.")
                 .addField("f!ping", "get a response from the bot.")
+                .addField("!8ball", "ask the ball a question to give answer on. ✔=yes ●=no answer X=no")
             message.channel.send(embed);
             break;
         case "ping":
